9d453c8238e508b72cefc3e3e1f6f8fefd2c056b
Pavla Beránková <berankovap@protonmail.com>
2023-04-06 15:35:57 +0200

    Initial Commit

---
+++ Wrap up_CWC/Wrap_up_CWCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wrap_up_CWCApp.swift
+//  Wrap up_CWC
+//
+//  Created by Pavla Beránková on 06.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Wrap_up_CWCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
